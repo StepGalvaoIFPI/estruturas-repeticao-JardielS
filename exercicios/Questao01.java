0e32771ff1f7c790c8a570c91ae800eab7ffa015
JardielS <157621545+JardielS@users.noreply.github.com>
2025-04-22 19:10:28 -0300

    Questao01.java
    up

--- exercicios/Questao01.java
+++ exercicios/Questao01.java
@@ -1,5 +1,15 @@
 public class Questao1{
     public static void main(String[] args) {
+        float base;
+        float area;
+        float altura;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite a base do triângulo: ");
+        base = scanner.nextFloat();
+        altura = scanner.nextFloat();
+        area = (base * altura) / 2;
+        System.out.println("A área do triângulo é: " + area);
         
-    
+}
+
 }
